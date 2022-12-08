fc011c094675cc10f85bcdf5287126b0642bfb1d
Hakan ÖZER <hakanozer02@gmail.com>
2022-12-08 14:52:23 +0300

    Security Config

--- REST/src/main/java/com/works/configs/SecurityConfig.java
+++ REST/src/main/java/com/works/configs/SecurityConfig.java
@@ -1,29 +1,44 @@
 package com.works.configs;
 
+import com.works.services.AdminDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final AdminDetailService adminDetailService;
+    final PasswordEncoder passwordEncoder;
 
     // username and password -> slq select
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(adminDetailService).passwordEncoder(passwordEncoder);
     }
 
     // basic auth vb -> role mapping
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().formLogin().disable();
+        http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/product/**").hasRole("product")
+        .antMatchers("/customer/**").hasRole("customer")
+        .and()
+        .csrf().disable().formLogin().disable();
+
         http.headers().frameOptions().disable();
     }
 
     /*
         ali@mail.com -> product
         veli@mail.com -> customer
-        zehre@mail.com -> product, customer
+        zehra@mail.com -> product, customer
      */
 }
--- REST/src/main/java/com/works/services/AdminDetailService.java
+++ REST/src/main/java/com/works/services/AdminDetailService.java
@@ -1,23 +1,27 @@
 package com.works.services;
 
 import com.works.entities.Admin;
+import com.works.entities.Role;
 import com.works.repositories.AdminRepository;
 import com.works.utils.ERest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AdminDetailService implements UserDetailsService {
 
     final AdminRepository adminRepository;
@@ -25,7 +29,29 @@ public class AdminDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<Admin> optionalAdmin = adminRepository.findByUsernameEqualsIgnoreCase(username);
+        if (optionalAdmin.isPresent()) {
+            Admin admin = optionalAdmin.get();
+            return new User(
+                    admin.getUsername(),
+                    admin.getPassword(),
+                    admin.getEnable(),
+                    true,
+                    true,
+                    true,
+                    parseRoles(admin.getRoles())
+            );
+        }else {
+            throw new UsernameNotFoundException("UserName Not Found");
+        }
+    }
+
+    private Collection<? extends GrantedAuthority> parseRoles(List<Role> roles) {
+        List<GrantedAuthority> ls = new ArrayList<>();
+        for(Role role : roles) {
+            ls.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return ls;
     }
 
     public ResponseEntity register(Admin admin) {
